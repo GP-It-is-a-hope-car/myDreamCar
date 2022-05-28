7d95cc74789ed849f7842c8ec4d23ac82f13475a
Sung Jun Cho <chosung0531@naver.com>
2022-05-28 20:38:00 +0900

    5.28 엔딩 추가 구현사항

--- TeamProject/Ending.cpp
+++ TeamProject/Ending.cpp
@@ -10,24 +10,41 @@ Ending::Ending()
 	texture_ = SDL_CreateTextureFromSurface(g_renderer, temp_surface);
 	SDL_FreeSurface(temp_surface);
 	source_rectangle_[0] = { 0,0,640,640 };
-	source_rectangle_[1] = { 640,0,1280,640 };
-	source_rectangle_[2] = { 1280,0,1920,640 };
+	source_rectangle_[1] = { 640,0,640,640 };
+	source_rectangle_[2] = { 1280,0,640,640 };
 	destination_rectangle_ = { 0,0,640,640 };
 
+	SDL_Surface* temp1_surface = IMG_Load("../../Resources/triangle.png");
+	start_texture = SDL_CreateTextureFromSurface(g_renderer, temp1_surface);
+	SDL_FreeSurface(temp1_surface);
+	start_source = { 0,0,200,200 };
+	start_dest = { 260,300,20,20 };
+	
 	SDL_Color white = { 0, 0, 0, 0 };
 	TTF_Font* font = TTF_OpenFont("../../Resources/bmjua.ttf", 50);
 	SDL_Surface* tmp_surface = TTF_RenderText_Blended(font, std::to_string((long long)metal_count).c_str(), white);
 	metal_texture_rect= { 0,0,150,100 };
 	metal_texture= SDL_CreateTextureFromSurface(g_renderer, tmp_surface);
 	SDL_FreeSurface(tmp_surface);
-	TTF_CloseFont(font);
+	
 	end_cur = 0;
 	end_num = 2;
+	re = 0;
+	
+	SDL_Color red = { 255, 0, 0, 0 };
+	SDL_Surface* tmp1_surface = TTF_RenderText_Blended(font, "YES", red);
+	re_source_rect = { 0,0,200,200 };
+	re_dest_rect = { 260,250,40,40 };
+	re_texture= SDL_CreateTextureFromSurface(g_renderer, tmp1_surface);
+	SDL_FreeSurface(tmp1_surface);
+	TTF_CloseFont(font);
 }
 
 Ending::~Ending()
 {
 	SDL_DestroyTexture(metal_texture);
+	SDL_DestroyTexture(start_texture);
+	SDL_DestroyTexture(re_texture);
 	SDL_DestroyTexture(texture_);
 }
 
@@ -43,9 +60,12 @@ void Ending::Update()
 void Ending::Render()
 {
 	SDL_Rect tmp_;
-	tmp_ = { 220,500,55,50 };
-	SDL_RenderCopy(g_renderer, metal_texture, &metal_texture_rect, &tmp_);
+	tmp_ = { 0,50,55,50 };
+	
 	SDL_RenderCopy(g_renderer, texture_, &source_rectangle_[end_cur], &destination_rectangle_);
+	SDL_RenderCopy(g_renderer, start_texture, &start_source, &start_dest);
+	SDL_RenderCopy(g_renderer, re_texture, &re_source_rect, &re_dest_rect);
+	SDL_RenderCopy(g_renderer, metal_texture, &metal_texture_rect, &tmp_);
 	SDL_RenderPresent(g_renderer);
 }
 
@@ -64,6 +84,22 @@ void Ending::HandleEvents()
 		case SDL_KEYDOWN:
 			if (event.key.keysym.sym == SDLK_SPACE) {
 				g_current_game_phase = PHASE_INTRO;
+			}
+			if (event.key.keysym.sym == SDLK_KP_ENTER) {
+				if (re == 0) {
+					g_current_game_phase = PHASE_INTRO;
+				}
+				else if(re==1) {
+					exit(0);
+				}
+			}
+			if (event.key.keysym.sym == SDLK_LEFT) {
+				start_dest.x = 260;
+				re = 0;
+			}
+			if (event.key.keysym.sym == SDLK_RIGHT) {
+				start_dest.x = 320;
+				re = 1;
 			}
 		case SDL_MOUSEBUTTONDOWN:
 
--- TeamProject/Ending.h
+++ TeamProject/Ending.h
@@ -12,14 +12,21 @@ public:
 	virtual void Render() override;
 
 private:
-	SDL_Texture* texture_; // the SDL_Texture 
-	SDL_Rect source_rectangle_[3]; // the rectangle for source image
-	SDL_Rect destination_rectangle_; // for destination
-	SDL_Texture* metal_texture; // 메탈 수 텍스트
+	SDL_Texture* texture_; 
+	SDL_Rect source_rectangle_[3];
+	SDL_Rect destination_rectangle_; 
+	SDL_Texture* metal_texture; 
 	SDL_Rect metal_texture_rect;
+	SDL_Texture* re_texture;
+	SDL_Rect re_source_rect;
+	SDL_Rect re_dest_rect;
 
+	SDL_Texture* start_texture;
+	SDL_Rect start_source; 
+	SDL_Rect start_dest; 
 	int end_num;
 	int end_cur;
+	int re;
 };
 
 
